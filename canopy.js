343953999ee45f558036d8835c5b0abb0e1324c2
Greg Prisament <greg@canopy.link>
2014-12-02 10:54:51 -0500

    Work in progress on new canopy.js javascript library

---
+++ canopy.js
@@ -0,0 +1,425 @@
+/*
+Canopy Javascript Client API
+
+CANOPY CLIENT OBJECT
+--------------------
+
+Initialize the library by creating a Canopy Client Object with:
+
+    canopy = new CanopyClient({
+        "cloud-host" : "ccs.canopy.link"
+    });
+
+TRACKING ACCOUNTS AND DEVICES
+-----------------------
+
+Before you can access a device or account's data, you must first "track" it.
+
+Each Canopy Client Object can "track" multiple Account and Device objects.  By
+"tracking" an object you can access and manipulate it within javascript.
+The tracking mechanism allows an efficient implementation to update (or
+"synchronize") all tracked objects with a single AJAX request.
+
+To track an account:
+
+    var acct = canopy.Account(username);
+
+To track the currently logged-in user (based on session cookies):
+
+    var acct = canopy.Account("me");
+
+To track a device:
+
+    var dev = canopy.Device(UUID);
+
+Once you have started tracking one or more objects, you can synchronize with
+the server using:
+
+    canopy.Sync({ onSuccess: function() {
+        // your code here
+    }});
+
+By default, Sync will synchronize all tracked Accounts and Devices.  You can
+synchronize specific objects with:
+
+    canopy.Sync({
+        accounts : [username],
+        devices: [uuid0, uuid1, ...],
+        onSuccess: function() {
+        }
+    });
+
+This form of the command will synchronize the specified devices even if they
+are not being tracked.
+
+Callbacks:
+--------------------
+onReady
+
+CanopyClient Callbacks:
+    "device-loaded"
+        Called when a new Device object is loaded for the first
+        time.
+
+Account Callbacks:
+    "device-loaded" - Called when a new Device object is loaded.
+
+ACCOUNTS
+--------------------
+
+If the user has authenticated with the Canopy Cloud Service (by logging in, for
+example), you can access their Account Object:
+
+    canopy.me
+
+    // Get username
+    canopy.me.username();
+
+    // Push changes
+    canopy.me.save({
+        onSuccess: function() {
+            alert("Account updated");
+        }
+    });
+
+    // Get a user's devices:
+    canopy.me.devices();
+
+    canopy.users();
+
+    // Get device
+    canopy.device("UUID");
+
+    canopy.device("UUID");
+
+CLOUD VARIABLES
+--------------------
+
+    device = canopy.device("UUID");
+
+    device.gps.longitude.Value = 4.0f;
+*/
+
+function SDDLParser() {
+    // Parses json Object into anonymous cloudvar struct
+    this.Parse = function(jsonObj) {
+        return this.ParseVar("inout struct __root__", propsJsonObj);
+    }
+
+    // Returns {value: SDDLVarDef, error: errorStringOrNull}
+    this.ParseVar = function(declString, propsJsonObj) {
+        var out = new SDDLVar();
+
+        // Parse declString
+        // For example: "out float32 temperature";
+        out.priv.direction = "out";
+        out.priv.datatype = "string";
+        out.priv.name = "temperature";
+
+        // Parse properties
+        for (var x in propsJsonObj) {
+            var val = propsJsonObj[x];
+
+            if (x == "description") {
+                out.priv.description = val;
+            }
+            else if (x == "min-value") {
+                out.priv.minValue = val;
+            }
+            else if (x == "max-value") {
+                out.priv.maxValue = val;
+            }
+            else if (x == "regex") {
+                out.priv.regex = val;
+            }
+            else if (x == "units") {
+                out.priv.units = val;
+            }
+            else if (x == "units") {
+                out.priv.units = val;
+            }
+            else {
+                // only do this if it parses to a var declaration
+                var child = self.ParseVar(x, val);
+                if (child.error != null) {
+                    return {value: null, error: child.error};
+                }
+            }
+        }
+        return {value: out, error: null};
+    }
+
+    function SDDLVar() {
+        this.priv = {};
+        this.priv.children = {};
+
+        this.Direction() {
+            return this.priv.direction;
+        }
+
+        this.ConcreteDirection() {
+            if (this.Direction == "inherit") {
+                return this.priv.parent.ConcreteDirection();
+            }
+            return this.Direction();
+        }
+
+        this.Description() {
+            return this.priv.description;
+        }
+        this.Datatype() {
+            return this.priv.datatype;
+        }
+
+        this.Name() {
+            return this.priv.name;
+        }
+
+        this.MinValue() {
+            return this.priv.minValue;
+        }
+
+        this.MaxValue() {
+            return this.priv.maxValue;
+        }
+
+        this.NumericDisplayHint() {
+            return this.priv.numericDisplayHint;
+        }
+
+        this.Regex() {
+            return this.priv.regex;
+        }
+
+        this.Units() {
+            return this.priv.units;
+        }
+    }
+
+}
+
+function CanopyClient(origSettings) {
+    var priv = {};
+
+    // map: username -> Account object
+    priv.trackedAccounts = {};
+    // map: uuid -> Account object
+    priv.trackedDevices = {};
+
+    function device(id) {
+        return priv.device
+    }
+
+    function onReady(fn) {
+        priv.onReady = fn;
+    }
+
+    // Synchronize with the server
+    this.Sync = function(params) {
+        var trackAccts = priv.trackedAccounts;
+        var trackDevices = priv.trackedDevices;
+
+        if (priv.trackedAccounts['me'] !== undefined) {
+            var acct = priv.trackedAccounts['me'];
+            acct.priv.email = "greg@greg.com";
+            acct.priv.username = "gregulator";
+            acct.priv.ready = true;
+            acct.priv.onChange();
+
+            acct.priv.devices = [
+                new CanopyDevice()
+            ];
+            acct.priv.onDeviceLoaded(acct.priv.devices[0]);
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            data: JSON.stringify({ "sync-accounts" : trackAccts, "sync-devices" : trackDevices}),
+            dataType : "json",
+            url: self.apiBaseUrl() + "/sync",
+            xhrFields: {
+                 withCredentials: true
+            },
+            crossDomain: true
+        })
+        .done(function(data, textStatus, jqXHR) {
+            // Update each synchronized account object
+            if (data['result'] == "ok") {
+                var acct = new CanopyAccount({
+                    username: data['username'],
+                    email: data['email']
+                });
+                if (params.onSuccess)
+                    params.onSuccess(acct);
+            } else {
+                if (data['error_type']) {
+                    if (params.onError)
+                        params.onError(data['error_type']);
+                }
+                else {
+                    if (params.onError)
+                        params.onError("unknown");
+                }
+            }
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            /* TODO: determine error */
+            if (params.onError)
+                params.onError("unknown");
+        });
+    }
+
+    this.Account = function(account) {
+        if (priv.trackedAccounts[account] === undefined) {
+            var acct = new CanopyAccount(account);
+            priv.trackedAccounts[account] = acct;
+            return acct;
+        } else {
+            return priv.trackedAccounts[account];
+        }
+    }
+
+    this.Device = function(device) {
+    }
+    
+    // fetchAccount (private)
+    // AJAX request for account details.
+    function fetchAccount(params) {
+        $.ajax({
+            type: "GET",
+            dataType : "json",
+            url: self.apiBaseUrl() + "/me",
+            xhrFields: {
+                 withCredentials: true
+            },
+            crossDomain: true
+        })
+        .done(function(data, textStatus, jqXHR) {
+            if (data['result'] == "ok") {
+                var acct = new CanopyAccount({
+                    username: data['username'],
+                    email: data['email']
+                });
+                if (params.onSuccess)
+                    params.onSuccess(acct);
+            } else {
+                if (data['error_type']) {
+                    if (params.onError)
+                        params.onError(data['error_type']);
+                }
+                else {
+                    if (params.onError)
+                        params.onError("unknown");
+                }
+            }
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            /* TODO: determine error */
+            if (params.onError)
+                params.onError("unknown");
+        });
+    }
+
+    // CanopyAccount
+    // This is a private "class" of CanopyClient to prevent the caller from
+    // calling the constructor.
+    function CanopyAccount(initObj) {
+        var priv = {};
+        this.priv = priv;
+        priv.ready = false;
+
+        this.Email = function() {
+            if (!priv.ready) {
+                return {"value" : "", "error" : "Account Not Ready"};
+            }
+            return {"value" : priv.email, "error" : null};
+        }
+
+        this.On = function(eventNames, callback) {
+            if (eventNames == "change") {
+                priv.onChange = callback;
+            } else if (eventNames == "device-loaded") {
+                priv.onDeviceLoaded = callback;
+            }
+        }
+
+        this.Username = function() {
+            if (!priv.ready) {
+                return {"value" : "", "error" : "Not Ready"};
+            }
+            return {"value" : priv.username, "error" : null};
+        }
+
+        this.fetchDevices = function(params) {
+            /* TODO: Filter to only show devices for this account */
+            $.ajax({
+                type: "GET",
+                dataType : "json",
+                url: self.apiBaseUrl() + "/devices",
+                xhrFields: {
+                     withCredentials: true
+                },
+                crossDomain: true
+            })
+            .done(function(data, textStatus, jqXHR) {
+                /* construct CanopyDevice objects */
+                var devices = [];
+                for (var i = 0; i < data.devices.length; i++) {
+                    var dev = new CanopyDevice(data.devices[i]);
+                    if (dev.friendlyName().substring(0, 10) != "FakeDevice") // HACK!
+                        devices.push(dev);
+                }
+                if (params.onSuccess)
+                    params.onSuccess(new CanopyDeviceList(devices));
+            })
+            .fail(function() {
+                if (params.onError)
+                    params.onError("unknown");
+            });
+        }
+    }
+
+    function CloudVar(params) {
+        var priv = {};
+        priv.value = undefined;
+        this.Value = function(newValue) {
+            if (newValue !== undefined) {
+                priv.value = newValue;
+                // mark dirty
+            }
+            return priv.value;
+        }
+    }
+
+    function CanopyDevice(params) {
+        this.vars = {
+            "temperature" : new CloudVar()
+        };
+    }
+
+    // Initialization.
+    /*this.fetchAccount({
+        onSuccess : function(devices) {
+        }
+    })*/
+}
+
+// Example program:
+/*
+var canopy = new CanopyClient();
+
+acct = canopy.Account("me");
+device = acct.Devices();
+
+canopy.TrackDevice(CANOPY_DEVICE_UUID);
+canopy.AutoSync(true);
+
+canopy.onChange(canopy.Device("uuid").);
+
+canopy.Sync({
+    onSuccess: function() {
+        canopy.Device("UUID")
+    }
+});
+*/
