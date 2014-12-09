b4902c09d64fd3317d6b1e9a8fbdabdbf3907bf1
Greg Prisament <greg@canopy.link>
2014-12-08 18:43:38 -0800

    Bring back CanopyDevice class

--- canopy.js
+++ canopy.js
@@ -73,24 +73,24 @@ example), you can access their Account Object:
     canopy.me
 
     // Get username
-    canopy.me.username();
+    canopy.me.Username();
 
     // Push changes
-    canopy.me.save({
+    canopy.me.Save({
         onSuccess: function() {
             alert("Account updated");
         }
     });
 
     // Get a user's devices:
-    canopy.me.devices();
+    canopy.me.Devices();
 
     canopy.users();
 
     // Get device
-    canopy.device("UUID");
+    canopy.Device("UUID");
 
-    canopy.device("UUID");
+    canopy.Device("UUID");
 
 CLOUD VARIABLES
 --------------------
@@ -201,6 +201,7 @@ function SDDLParser() {
 function CanopyClient(origSettings) {
     var self=this;
     this.priv = {};
+    this.me = null;
 
     // map: username -> Account object
     this.priv.trackedAccounts = {};
@@ -265,6 +266,46 @@ function CanopyClient(origSettings) {
         });
     }
 
+    this.CreateDevice = function(params) {
+        $.ajax({
+            type: "POST",
+            dataType : "json",
+            contentType: 'text/plain; charset=utf-8', /* Needed for safari */
+            url: self.ApiBaseUrl() + "/create_device",
+            data: JSON.stringify({
+                device_name: params.deviceName,
+                quanitity: params.quantity,
+            }),
+            xhrFields: {
+                 withCredentials: true
+            },
+            crossDomain: true
+        })
+        .done(function(data) {
+            /* Initialize canopyClient object */
+            /*self.devices = new CanopyDeviceList([]);
+            self.account = new CanopyAccount({
+                username: params.username,
+                email: params.email
+            });*/
+            if (data['result'] == "ok") {
+                if (params.onSuccess != null)
+                    params.onSuccess();
+            } 
+            else {
+                if (params.onError != null)
+                    params.onError(data['error']);
+            }
+        })
+        .fail(function(XMLHttpRequest, textStatus, errorThrown) {
+            console.log(XMLHttpRequest);
+            console.log(textStatus);
+            console.log(errorThrown);
+            if (params.onError != null)
+                params.onError();
+        });
+    }
+
     this.Login = function(params) {
         /* TODO: proper error handlilng */
         /* TODO: response needs to include username & email */
@@ -281,25 +322,13 @@ function CanopyClient(origSettings) {
         })
         .done(function(data, textStatus, jqXHR) {
             if (data['result'] == "ok") {
-                /*var acct = new CanopyAccount({
+                var acct = new CanopyAccount({
                     username: data['username'],
                     email: data['email']
                 });
-                acct.fetchDevices({
-                    onSuccess: function(deviceList) {
-                        self.devices = deviceList;
-                        if (params.onSuccess)
-                            params.onSuccess(acct);
-                    },
-                    onError: function() {
-                        if (params.onError)
-                            params.onError("unknown");
-                    }
-                });
-                self.account = acct;*/
+                self.account = acct;
                 if (params.onSuccess)
-                    params.onSuccess();
-                
+                    params.onSuccess(acct);
             }
             else {
                 if (params.onError)
@@ -325,6 +354,26 @@ function CanopyClient(origSettings) {
                 if (params.onError)
                     params.onError("unknown");
             }
+        });
+    }
+
+    this.Logout = function(params) {
+        $.ajax({
+            type: "POST",
+            dataType : "json",
+            url: self.ApiBaseUrl() + "/logout",
+            xhrFields: {
+                 withCredentials: true
+            },
+            crossDomain: true
+        })
+        .done(function() {
+            if (params.onSuccess)
+                params.onSuccess();
+        })
+        .fail(function() {
+            if (params.onError)
+                params.onError();
         });
     }
 
@@ -417,8 +466,19 @@ function CanopyClient(origSettings) {
                     username: data['username'],
                     email: data['email']
                 });
-                if (params.onSuccess)
-                    params.onSuccess(acct);
+                self.me = acct;
+                acct.fetchDevices({
+                    onSuccess: function(deviceList) {
+                        self.devices = deviceList;
+                        self.me.devices = deviceList;
+                        if (params.onSuccess)
+                            params.onSuccess(acct);
+                    },
+                    onError: function() {
+                        if (params.onError)
+                            params.onError("unknown");
+                    }
+                });
             } else {
                 if (data['error_type']) {
                     if (params.onError)
@@ -443,7 +503,8 @@ function CanopyClient(origSettings) {
     function CanopyAccount(initObj) {
         var priv = {};
         this.priv = priv;
-        priv.ready = false;
+        priv.ready = true;
+        priv.username = initObj.username;
 
         this.Email = function() {
             if (!priv.ready) {
@@ -472,7 +533,7 @@ function CanopyClient(origSettings) {
             $.ajax({
                 type: "GET",
                 dataType : "json",
-                url: self.apiBaseUrl() + "/devices",
+                url: self.ApiBaseUrl() + "/me/devices",
                 xhrFields: {
                      withCredentials: true
                 },
@@ -483,8 +544,7 @@ function CanopyClient(origSettings) {
                 var devices = [];
                 for (var i = 0; i < data.devices.length; i++) {
                     var dev = new CanopyDevice(data.devices[i]);
-                    if (dev.friendlyName().substring(0, 10) != "FakeDevice") // HACK!
-                        devices.push(dev);
+                    devices.push(dev);
                 }
                 if (params.onSuccess)
                     params.onSuccess(new CanopyDeviceList(devices));
@@ -508,10 +568,171 @@ function CanopyClient(origSettings) {
         }
     }
 
-    function CanopyDevice(params) {
-        this.vars = {
-            "temperature" : new CloudVar()
-        };
+    /*
+     * CanopyDevice
+     *
+     * This is a private "class" of CanopyClient to prevent the caller from
+     * calling the constructor.
+     */
+    function CanopyDevice(initObj) {
+        var self=this;
+        /*var result = ParseResponse(self, initObj.sddl_class, initObj.property_values);
+        if (result.error != null) {
+            console.log(result.error);
+        }
+        var classInstance = result.instance;
+
+        this.properties = classInstance.properties;*/
+
+        this.id = function() {
+            return initObj.device_id;
+        }
+        this.UUID = this.id;
+
+        this.FriendlyName = function() {
+            return initObj.friendly_name;
+        }
+
+        this.notifications = function() {
+            // TODO: Wrap in object?
+            return initObj.notifications;
+        }
+
+        this.locationNote = function() {
+            return initObj.location_note ? initObj.location_note : "Home";
+        }
+
+        this.sddlClass = function() {
+            return new SDDLClass(initObj.sddl_class);
+        }
+
+        /*
+         *  params:
+         *      friendlyName
+         *      locationNote
+         *      onSuccess
+         *      onError
+         */
+        this.setSettings = function(params) {
+            obj = {
+                __friendly_name: params.friendlyName,
+                __location_note: params.locationNote
+            };
+            $.ajax({
+                type: "POST",
+                dataType : "json",
+                url: selfClient.apiBaseUrl() + "/device/" + self.id(),
+                data: JSON.stringify(obj),
+                xhrFields: {
+                     withCredentials: true
+                },
+                crossDomain: true
+            })
+            .done(function() {
+                if (params.onSuccess)
+                    params.onSuccess();
+            })
+            .fail(function() {
+                if (params.onError)
+                    params.onError();
+            });
+        }
+
+        /*
+         * params:
+         *  sddlObj -- The property to add/update.
+         *  onSuccess
+         *  onError
+         */
+        this.updateSDDL = function(params) {
+            obj = {
+                "__sddl_update" : params.sddlObj
+            };
+            $.ajax({
+                type: "POST",
+                dataType : "json",
+                url: selfClient.apiBaseUrl() + "/device/" + self.id(),
+                data: JSON.stringify(obj),
+                xhrFields: {
+                     withCredentials: true
+                },
+                crossDomain: true
+            })
+            .done(function() {
+                if (params.onSuccess)
+                    params.onSuccess();
+            })
+            .fail(function() {
+                if (params.onError)
+                    params.onError();
+            });
+        }
+
+        this.beginControlTransaction = function() {
+        }
+
+        /* Lists accounts who have permission to access this */
+        this.permissions = function() {
+        }
+
+        this.share = function(params) {
+        }
+
+        this.setPermissions = function(params) {
+        }
+
+        this.IsConnected = function() {
+            return initObj.connected;
+        }
+    }
+
+    /*
+     * <devices> is list of CanopyDevice objects.
+     */
+    function CanopyDeviceList(devices) {
+
+        this.Filter = function(options) {
+            filteredDevices = [];
+            if (options['connected'] === true) {
+                for (i = 0; i < devices.length; i++) {
+                    if (devices[i].IsConnected()) {
+                        filteredDevices.push(devices[i]);
+                    }
+                }
+                return new CanopyDeviceList(filteredDevices);
+            }
+            else if (options['connected'] === false) {
+                for (i = 0; i < devices.length; i++) {
+                    if (!devices[i].IsConnected()) {
+                        filteredDevices.push(devices[i]);
+                    }
+                }
+                return new CanopyDeviceList(filteredDevices);
+            }
+        }
+
+        this.Connected = function() {
+            return this.Filter({connected: true});
+        }
+
+        this.Disconnected = function() {
+            return this.Filter({connected: false});
+        }
+
+        this.Count = function(options) {
+            return this.Filter(options).length;
+        }
+
+        /* simulate array */
+        this.length = devices.length;
+        for (var i = 0; i < devices.length; i++) {
+            this[i] = devices[i];
+        }
+
+        /* simulate map */
+        for (var i = 0; i < devices.length; i++) {
+            this[devices[i].id()] = devices[i];
+        }
     }
 
     // Initialization
@@ -526,7 +747,6 @@ function CanopyClient(origSettings) {
             }
         })
     });
-
 }
 
 // Example program:
